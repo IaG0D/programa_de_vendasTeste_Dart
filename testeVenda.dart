84ab04d9307084e2acde31dca7e21920b7f8d695
IaG0D <iagovalimpereira12@hotmail.com>
2021-11-20 22:54:25 -0300

    Primeiro Commit

---
+++ testeVenda.dart
@@ -0,0 +1,52 @@
+import 'cliente.dart';
+import 'venda.dart';
+import 'produto.dart';
+import 'venda_item.dart';
+
+main(){
+  var venda = new Venda(
+    cliente: Cliente(
+      cpf: '151.168.586.77', 
+      nome: 'Iago'
+      ),
+
+      itens:<VendaItem> [
+        VendaItem(
+          quantidade: 3,
+          produto: Produto(
+            codigo: 2555,
+            nome: 'Coca',
+            preco: 6.75,
+            desconto: 0.10
+          )
+
+        ),
+        VendaItem(
+          quantidade: 9,
+          produto: Produto(
+            codigo: 2451,
+            nome: 'Batata',
+            preco: 1.90,
+            desconto: 0.05
+          )
+
+        ),
+        VendaItem(
+          quantidade: 10,
+          produto: Produto(
+            codigo: 2341,
+            nome: 'Maçã',
+            preco: 1.75,
+            desconto: 0.0
+          )
+
+        )
+      ]
+  );
+    
+    print("O valor total é: ${venda.valorTotal}");
+
+    print(venda.itens![0].produto!.nome);
+    print(venda.cliente!.nome);
+  
+}
